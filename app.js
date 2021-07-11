dc39a65d84a64ae37332e19c464198ae9eebea03
YellowFlash2012 <cyclop9@hotmail.com>
2021-07-11 16:52:49 +0100

    Add index.html app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const fullDate = document.querySelector('.full-date');
 
 fullDate.innerHTML = new Date().getFullYear();
+
+const themeChangeBtn = document.querySelector('.dark-light');
+const themeIcon = document.querySelector('.theme-icon');
+
+themeChangeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    
+});
