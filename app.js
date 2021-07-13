69120c6edd93c299e5bf9c85a7a5f4f4081dece5
YellowFlash2012 <cyclop9@hotmail.com>
2021-07-13 17:20:30 +0100

    Add index.html style.css app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const fullDate = document.querySelector('.full-date');
 
+const img = document.querySelector('.one img');
+const p = document.querySelector('.one p');
+const arrowIcon = document.querySelector('.one i');
+
 fullDate.innerHTML = new Date().getFullYear();
 
 const themeChangeBtn = document.querySelector('.dark-light');
@@ -9,3 +13,5 @@ themeChangeBtn.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     
 });
+
+//hover effect on img
