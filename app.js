dbb27265a7223b5b8c5392b7ec88680b31550a5d
YellowFlash2012 <cyclop9@hotmail.com>
2021-07-27 20:53:35 +0100

    Add index.html style.css app.js

--- app.js
+++ app.js
@@ -24,17 +24,28 @@ const weekdays = [
 
 
 let fullYear = document.querySelector('.year');
-const fullDate = document.querySelector('.full-date')
+const fullDate = document.querySelector('.full-date');
+const dailyDate = document.querySelectorAll('.daily-date');
+
 const header = document.getElementById('home');
 const backTop = document.querySelector('.back-top');
 
-const img = document.querySelector('.one img');
-const p = document.querySelector('.one p');
-const arrowIcon = document.querySelector('.one i');
+const OpenHamburgerBtn = document.querySelector('.open-hamburger-menu');
+const closeHamburgerMenu = document.querySelector('.close-hamburger-menu');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+const oneGroups = [...document.querySelectorAll('.one-group')];
+
+
+const img = document.querySelector('.one-group img');
+const p = [...document.querySelectorAll('.one-group p')];
+const arrowIcon = [...document.querySelectorAll('.one-group i')];
+
+const stickyBanner = document.querySelector('.banner-ad');
 
 // dynamic full year setup
 fullYear.innerText = new Date().getFullYear();
-console.log(fullYear);
+
 
 //dynamic full date setup
 let day = weekdays[new Date().getDay()];
@@ -43,6 +54,11 @@ const date = new Date().getDate();
 let year = new Date().getFullYear();
 
 fullDate.innerText = `${day}, ${month} ${date}, ${year}`;
+
+//dynamic daily date setup
+dailyDate.forEach((el) => {
+    el.textContent = `${month} ${date}, ${year}`;
+})
 
 //dark-light theme setup
 const themeChangeBtn = document.querySelector('.dark-light');
@@ -71,5 +87,66 @@ window.addEventListener('scroll', () => {
         backTop.style.visibility = 'visible';
     } else {
         backTop.style.visibility = 'hidden';
+    }
+})
+
+//hamburger menu
+OpenHamburgerBtn.addEventListener('click', () => {
+    hamburgerMenu.style.display = 'block';
+    OpenHamburgerBtn.innerHTML = `<i class="fas fa-times fa-2x"></i>`;
+    
+});
+
+closeHamburgerMenu.addEventListener('click', () => {
+    console.log(closeHamburgerMenu);
+    hamburgerMenu.style.display = 'none';
+    closeHamburgerMenu.innerHTML = `<i class="fas fa-bars fa-2x"></i>`;
+});
+
+//weekly-top-5 img dynamic transition of num & arrow setup
+oneGroups.forEach((item) => {
+    item.addEventListener('mouseenter', (e) => {
+        if (e.target === item) {
+            p.forEach((item) => {
+                item.style.transform = 'translateY(-290px)';
+            
+        });
+        
+            arrowIcon.forEach((item) => {
+                item.style.transform = 'translateY(2px)';
+                item.style.opacity = '1';
+            });
+        }
+        // p.forEach((item) => {
+        //     item.style.transform = 'translateY(-290px)';
+            
+        // });
+        
+        // arrowIcon.forEach((item) => {
+        //     item.style.transform = 'translateY(2px)';
+        //     item.style.opacity = '1';
+        // });
+        
+    });
+
+    item.addEventListener('mouseleave', () => {
+        p.forEach((item) => {
+            item.style.transform = 'translateY(0px)';
+        });
+        
+        arrowIcon.forEach((item) => {
+            item.style.opacity = '0';
+        });
+    })
+});
+
+//sticky banner ad setup
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.pageYOffset;
+
+    if (scrollHeight >= 2278 && scrollHeight <= 2900) {
+        stickyBanner.classList.add('fixed-banner-ad');
+    } else {
+        stickyBanner.classList.remove('fixed-banner-ad');
     }
 })
